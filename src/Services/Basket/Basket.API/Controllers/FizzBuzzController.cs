ea7bea290d02a08d5568971972a35ac12c64c798
Samuel Leclerc <samuel.leclerc@quest.com>
2022-08-04 16:39:54 -0400

    Fix

--- src/Services/Basket/Basket.API/Controllers/FizzBuzzController.cs
+++ src/Services/Basket/Basket.API/Controllers/FizzBuzzController.cs
@@ -37,3 +37,21 @@ namespace Basket.API.Controllers
         }
     }
 }
+
+
+// Expected Output if num = 15:
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
