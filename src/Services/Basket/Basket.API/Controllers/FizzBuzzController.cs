87f855afb32248d6c0850d1afc2b7c6d0054042f
Samuel Leclerc <samuel.leclerc@quest.com>
2022-08-04 15:56:25 -0400

    Added FizzBuzzController

---
+++ src/Services/Basket/Basket.API/Controllers/FizzBuzzController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class FizzBuzzController : ControllerBase
+    {
+        [HttpGet("{num}")]
+        public async Task<string> FizzBuzz(int num)
+        {
+            var result = string.Empty;
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    result = result + "FizzBuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    result = result + "Fizz";
+                }
+                else if(i % 5 == 0)
+                {
+                    result = result + "Buzz";
+                }
+                else
+                {
+                    result = result + i.ToString();
+                }
+
+                result = result + "\n";
+            }
+
+            return result;
+        }
+    }
+}
